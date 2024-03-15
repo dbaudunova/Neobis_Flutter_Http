9cc7c335df91a1abf7389432bdd76ed6ae756162
Dilnaz27 <dbaudunova@gmail.com>
2024-03-16 00:22:21 +0600

    style: remove empty line, feat: add listview that displays posts

--- lib/data/api_client/api_client.dart
+++ lib/data/api_client/api_client.dart
@@ -1,7 +1,5 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
-
 import '../model/post_model.dart';
 
 class ApiClient {
--- lib/presentation/pages/post.dart
+++ lib/presentation/pages/post.dart
@@ -10,16 +10,58 @@ class Post extends StatefulWidget {
 }
 
 class _PostState extends State<Post> {
-  late Future<List<PostModel>> posts;
+  late Future<List<PostModel>> _posts;
 
   @override
   void initState() {
     super.initState();
-    posts = ApiClient().getPosts();
+    _posts = ApiClient().getPosts();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: FutureBuilder<List<PostModel>>(
+        future: _posts,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          } else {
+            List<PostModel>? posts = snapshot.data;
+            return ListView.builder(
+                itemCount: posts?.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Material(
+                      elevation: 4.0,
+                      borderRadius: BorderRadius.circular(16.0),
+                      color: Colors.blue[100],
+                      child: ListTile(
+                        title: Text(
+                          posts![index].title,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                        subtitle: Text(
+                          posts[index].body,
+                          style: const TextStyle(fontSize: 16),
+                        ),
+                      ),
+                    ),
+                  );
+                });
+          }
+        },
+      ),
+    );
   }
 }
